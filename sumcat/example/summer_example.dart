ad9672a74f0b8487903e984338bed4c48986e5ae
JustDooooIt <844730658@qq.com>
2024-04-08 02:22:31 +0800

    Completion of basic functions

--- sumcat/example/summer_example.dart
+++ sumcat/example/summer_example.dart
@@ -2,18 +2,6 @@ import 'dart:io';
 import 'package:sumcat/sumcat.dart';
 
 void main() {
-handler
-
-
-
-
-历史记录
-
-
-请选择目标语言。当前已选择：
-中文
-
-
   var app = createApplication();
   app.get("/test", (HttpRequest req, HttpResponse res, Function next) {
     res.write(req.uri);

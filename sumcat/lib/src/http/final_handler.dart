77f8d9b07ddd66634127460413d73c9249f58ec7
JustDooooIt <844730658@qq.com>
2024-04-11 23:05:57 +0800

    fix bug

--- sumcat/lib/src/http/final_handler.dart
+++ sumcat/lib/src/http/final_handler.dart
@@ -3,7 +3,10 @@ part of http;
 Future<void> httpFinalHandler(
     HttpRequestWrapper req, HttpResponseWrapper res, String? err) async {
   var code = res._inner.statusCode;
-  //res.writeln('status code: $code');
+  if (code > 400) {
+    res.inner.writeln('status code: $code');
+    res.inner.writeln('err: $err');
+  }
   await res.inner.close();
 }
 
@@ -11,4 +14,6 @@ Future<void> webSocketFinalHandler(
     HttpRequestWrapper req, HttpResponseWrapper res, String? err) async {
   var internalReq = req as HttpRequestWrapperInternal;
   var code = res._inner.statusCode;
+  // var _httpRequest = (req as HttpRequestWrapperInternal);
+  // await _httpRequest.ws?.close();
 }
